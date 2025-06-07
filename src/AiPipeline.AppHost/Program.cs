53ec60174bbd275ffb1eefa2bfeafed44ce87ea0
Michal Musil <musilmichal16@gmail.com>
2025-06-07 10:38:19 +0200

    refactor: rename and restructure solution

---
+++ src/AiPipeline.AppHost/Program.cs
@@ -0,0 +1,23 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var preprocessingService = builder.AddProject<AiPipeline_TireOcr_Preprocessing_WebApi>("PreprocessingService")
+    .WithHttpsHealthCheck("/health");
+
+var ocrService = builder.AddProject<AiPipeline_TireOcr_Ocr_WebApi>("OcrService")
+    .WithHttpsHealthCheck("/health");
+
+var postprocessingService = builder.AddProject<AiPipeline_TireOcr_Postprocessing_WebApi>("PostprocessingService")
+    .WithHttpsHealthCheck("/health");
+
+var runnerPrototype = builder.AddProject<AiPipeline_TireOcr_RunnerPrototype>("RunnerPrototype")
+    .WithHttpsHealthCheck("/health")
+    .WithReference(preprocessingService)
+    .WaitFor(preprocessingService)
+    .WithReference(ocrService)
+    .WaitFor(ocrService)
+    .WithReference(postprocessingService)
+    .WaitFor(postprocessingService);
+
+builder.Build().Run();
