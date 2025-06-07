66d83f2e029bd4f6002f168e6d3d94085edf037e
Michal Musil <musilmichal16@gmail.com>
2025-06-07 21:28:33 +0200

    feat(orchestration): add node advertisement contract

---
+++ src/AiPipeline.Orchestration/AiPipeline.Orchestration.Contracts/Events/NodeAdvertisement/NodeAdvertised.cs
@@ -0,0 +1,7 @@
+namespace AiPipeline.Orchestration.Contracts.Events.NodeAdvertisement;
+
+public record NodeAdvertised
+{
+    public required string NodeName { get; init; }
+    public required IEnumerable<ProcedureDescriptor> Procedures { get; init; }
+}
---
+++ src/AiPipeline.Orchestration/AiPipeline.Orchestration.Contracts/Events/NodeAdvertisement/ProcedureDescriptor.cs
@@ -0,0 +1,11 @@
+using AiPipeline.Orchestration.Contracts.Schema;
+
+namespace AiPipeline.Orchestration.Contracts.Events.NodeAdvertisement;
+
+public record ProcedureDescriptor
+{
+    public required string Name { get; init; }
+    public required int SchemaVersion { get; init; } = 1;
+    public required IApElement Input { get; init; }
+    public required IApElement Output { get; init; }
+}
