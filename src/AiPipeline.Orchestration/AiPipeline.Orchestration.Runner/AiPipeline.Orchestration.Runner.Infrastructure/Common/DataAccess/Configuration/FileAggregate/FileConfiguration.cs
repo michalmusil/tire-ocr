0afff3a7046670b598e2f211431f90b05130f7bc
Michal Musil <musilmichal16@gmail.com>
2025-07-13 17:37:16 +0200

    feat(orchestration): add file aggregate entity configuration

---
+++ src/AiPipeline.Orchestration/AiPipeline.Orchestration.Runner/AiPipeline.Orchestration.Runner.Infrastructure/Common/DataAccess/Configuration/FileAggregate/FileConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AiPipeline.Orchestration.Runner.Infrastructure.Common.DataAccess.Configuration.FileAggregate;
+
+public class FileConfiguration : IEntityTypeConfiguration<Domain.FileAggregate.File>
+{
+    public void Configure(EntityTypeBuilder<Domain.FileAggregate.File> builder)
+    {
+        builder.HasKey(f => f.Id);
+        builder.Property(f => f.Id)
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Property(f => f.FileStorageScope)
+            .IsRequired();
+        
+        builder.Property(f => f.StorageProvider)
+            .IsRequired();
+        
+        builder.Property(f => f.Path)
+            .IsRequired();
+        
+        builder.Property(f => f.ContentType)
+            .IsRequired();
+    }
+}
