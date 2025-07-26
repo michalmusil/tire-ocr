254eb39b2791795b6b1c603c9af45ea63cdcd34f
Michal Musil <musilmichal16@gmail.com>
2025-07-26 18:50:53 +0200

    feat(postprocessing): implement postprocessing procedure in messaging project

--- src/AiPipeline.TireOcr/AiPipeline.TireOcr.Postprocessing/AiPipeline.TireOcr.Postprocessing.Messaging/Procedures/PerformTireCodePostprocessingProcedure.cs
+++ src/AiPipeline.TireOcr/AiPipeline.TireOcr.Postprocessing/AiPipeline.TireOcr.Postprocessing.Messaging/Procedures/PerformTireCodePostprocessingProcedure.cs
@@ -2,6 +2,8 @@ using AiPipeline.Orchestration.Shared.All.Contracts.Commands.RunPipelineStep;
 using AiPipeline.Orchestration.Shared.All.Contracts.Schema;
 using AiPipeline.Orchestration.Shared.All.Contracts.Schema.Properties;
 using AiPipeline.Orchestration.Shared.Nodes.Procedures;
+using MediatR;
+using TireOcr.Postprocessing.Application.Queries.TireCodePostprocessing;
 using TireOcr.Shared.Result;
 
 namespace AiPipeline.TireOcr.Postprocessing.Messaging.Procedures;
@@ -11,6 +13,7 @@ public class PerformTireCodePostprocessingProcedure : IProcedure
     public string Id => "PerformTireCodePostprocessing";
     public int SchemaVersion => 1;
     public IApElement InputSchema => ApString.Template();
+
     public IApElement OutputSchema => new ApObject(
         properties: new()
         {
@@ -30,8 +33,48 @@ public class PerformTireCodePostprocessingProcedure : IProcedure
         ]
     );
 
-    public Task<DataResult<IApElement>> ExecuteAsync(IApElement input, List<FileReference> fileReferences)
+    private readonly IMediator _mediator;
+
+    public PerformTireCodePostprocessingProcedure(IMediator mediator)
     {
-        return Task.FromResult(DataResult<IApElement>.Success(OutputSchema));
+        _mediator = mediator;
+    }
+
+    public async Task<DataResult<IApElement>> ExecuteAsync(IApElement input, List<FileReference> fileReferences)
+    {
+        var schemaCompatible = InputSchema.HasCompatibleSchemaWith(input);
+        if (!schemaCompatible)
+            return DataResult<IApElement>.Invalid(
+                $"Procedure '{nameof(PerformTireCodePostprocessingProcedure)}' failed: input schema is not compatible");
+
+        var inputString = (ApString)input;
+        var rawTireCode = inputString.Value;
+
+        var query = new TireCodePostprocessingQuery(rawTireCode);
+        var result = await _mediator.Send(query);
+
+        return result.Map(
+            onSuccess: data => DataResult<IApElement>.Success(
+                new ApObject(
+                    properties: new()
+                    {
+                        { "RawCode", new ApString(data.RawCode) },
+                        { "PostprocessedTireCode", new ApString(data.PostprocessedTireCode) },
+                        { "VehicleClass", new ApString(data.VehicleClass ?? "") },
+                        { "Width", new ApDecimal(data.Width ?? default) },
+                        { "AspectRatio", new ApDecimal(data.AspectRatio ?? default) },
+                        { "Construction", new ApString(data.Construction ?? "") },
+                        { "Diameter", new ApDecimal(data.Diameter ?? default) },
+                        { "LoadIndex", new ApString(data.LoadIndex ?? "") },
+                        { "SpeedRating", new ApString(data.SpeedRating ?? "") }
+                    },
+                    nonRequiredProperties:
+                    [
+                        "VehicleClass", "Width", "AspectRatio", "Construction", "Diameter", "LoadIndex", "SpeedRating"
+                    ]
+                )
+            ),
+            onFailure: DataResult<IApElement>.Failure
+        );
     }
 }
