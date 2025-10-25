89b494a107f1c3a3ee6ce0464af2e259a140b6b9
Michal Musil <musilmichal16@gmail.com>
2025-10-25 22:55:51 +0200

    feat(preprocessing): implement new type of additive overlap image slicing

--- src/AiPipeline.TireOcr/AiPipeline.TireOcr.Preprocessing/AiPipeline.TireOcr.Preprocessing.Application/Services/IImageSlicerService.cs
+++ src/AiPipeline.TireOcr/AiPipeline.TireOcr.Preprocessing/AiPipeline.TireOcr.Preprocessing.Application/Services/IImageSlicerService.cs
@@ -5,7 +5,35 @@ namespace TireOcr.Preprocessing.Application.Services;
 
 public interface IImageSlicerService
 {
-    public Task<DataResult<IEnumerable<Image>>> SliceImage(
+    /// <summary>
+    /// Splits an image into slices based on specified slice size and overlap.
+    /// The image is sliced horizontally if the sliceSize width is less than image width and vertically if sliceSize
+    /// height is less than image height. Each slice has the dimension of the specified sliceSize, or less (the last
+    /// slice usually has to be smaller, unless the sliceSize dimension is an equal division of the image dimension).
+    ///
+    /// The slices also overlap each other according to the specified xOverlapRatio and yOverlapRatio. Each ratio
+    /// is between 0 and 1 and specifies a percentage of the sliceSize dimension in which individual slices should
+    /// overlap (in a given axis). 
+    /// </summary>
+    public Task<DataResult<IEnumerable<Image>>> SliceImageNormalOverlap(
+        Image image,
+        ImageSize sliceSize,
+        double xOverlapRatio,
+        double yOverlapRatio
+    );
+
+    /// <summary>
+    /// Splits an image into slices based on specified slice size and overlap.
+    /// The image is sliced horizontally if the sliceSize width is less than image width and vertically if sliceSize
+    /// height is less than image height. Each slice has the dimension of the specified sliceSize, or less (the last
+    /// slice usually has to be smaller, unless the sliceSize dimension is an equal division of the image dimension).
+    ///
+    /// In this method as opposed to the 'SliceImage', the overlap is added to the slices sides - meaning if I for example
+    /// provide x overlap of 0.2 and y overlap of 0.1, each slice will be enlarged by 20% of its width on left and right
+    /// and 10% of its height on top and bottom. The overlap is not added in case it would overflow the original image
+    /// dimension. 
+    /// </summary>
+    public Task<DataResult<IEnumerable<Image>>> SliceImageAdditiveOverlap(
         Image image,
         ImageSize sliceSize,
         double xOverlapRatio,
--- src/AiPipeline.TireOcr/AiPipeline.TireOcr.Preprocessing/AiPipeline.TireOcr.Preprocessing.Infrastructure/Services/OpenCvImageSlicerService.cs
+++ src/AiPipeline.TireOcr/AiPipeline.TireOcr.Preprocessing/AiPipeline.TireOcr.Preprocessing.Infrastructure/Services/OpenCvImageSlicerService.cs
@@ -8,7 +8,7 @@ namespace TireOcr.Preprocessing.Infrastructure.Services;
 
 public class OpenCvImageSlicerService : IImageSlicerService
 {
-    public async Task<DataResult<IEnumerable<Image>>> SliceImage(
+    public async Task<DataResult<IEnumerable<Image>>> SliceImageNormalOverlap(
         Image image,
         ImageSize sliceSize,
         double xOverlapRatio,
@@ -42,6 +42,57 @@ public class OpenCvImageSlicerService : IImageSlicerService
                     var ymin = (int)y;
                     var xmax = (int)Math.Min(x + realSliceWidth, width);
                     var ymax = (int)Math.Min(y + realSliceHeight, height);
+
+                    var rect = new Rect(xmin, ymin, xmax - xmin, ymax - ymin);
+                    slices.Add(new Mat(inputImage, rect));
+                }
+            }
+
+            var resultImages = new List<Image>();
+            for (var i = 0; i < slices.Count; i++)
+            {
+                var slice = slices[i];
+                var imageName = $"{i + 1}_{image.Name}";
+                resultImages.Add(slice.ToDomain(imageName));
+
+                slice.Dispose();
+            }
+
+            return DataResult<IEnumerable<Image>>.Success(resultImages);
+        }
+        catch (Exception ex)
+        {
+            return DataResult<IEnumerable<Image>>.Failure(new Failure(500, "Failed to slice image."));
+        }
+    }
+
+    public async Task<DataResult<IEnumerable<Image>>> SliceImageAdditiveOverlap(Image image, ImageSize sliceSize,
+        double xOverlapRatio, double yOverlapRatio)
+    {
+        try
+        {
+            using var inputImage = image.ToCv2();
+            var height = inputImage.Height;
+            var width = inputImage.Width;
+
+            var realSliceWidth = sliceSize.Width;
+            var realSliceHeight = sliceSize.Height;
+
+            var overlapWidth = xOverlapRatio * realSliceWidth;
+            var overlapHeight = yOverlapRatio * realSliceHeight;
+
+            var startingXs = GenerateRange(0, width, realSliceWidth);
+            var startingYs = GenerateRange(0, height, realSliceHeight);
+
+            var slices = new List<Mat>();
+            foreach (var y in startingYs)
+            {
+                foreach (var x in startingXs)
+                {
+                    var xmin = (int)Math.Max(0, x - overlapWidth);
+                    var ymin = (int)Math.Max(0, y - overlapHeight);
+                    var xmax = (int)Math.Min(x + realSliceWidth + overlapWidth, width);
+                    var ymax = (int)Math.Min(y + realSliceHeight + overlapHeight, height);
 
                     var rect = new Rect(xmin, ymin, xmax - xmin, ymax - ymin);
                     slices.Add(new Mat(inputImage, rect));
