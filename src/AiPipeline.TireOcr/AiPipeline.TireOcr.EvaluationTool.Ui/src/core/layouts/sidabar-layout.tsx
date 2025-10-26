9be92f5c822dd3803a9798b6c009a70cec75e6ed
Michal Musil <musilmichal16@gmail.com>
2025-10-26 19:40:23 +0100

    feat(evaluation-tool): use shad-cn sidebar

--- src/AiPipeline.TireOcr/AiPipeline.TireOcr.EvaluationTool.Ui/src/core/layouts/sidabar-layout.tsx
+++ src/AiPipeline.TireOcr/AiPipeline.TireOcr.EvaluationTool.Ui/src/core/layouts/sidabar-layout.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import AppSidebar from "@/core/components/app-sidebar";
 import { ThemeProvider } from "../components/theme-provider";
+import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
+import AppSidebar from "../components/app-sidebar";
+import { Separator } from "../components/ui/separator";
 
 const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex flex-col md:flex-row h-screen">
+      <SidebarProvider>
         <AppSidebar />
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <main className="flex-1 overflow-x-hidden overflow-y-auto">
-            <div className="container mx-auto px-6 py-8">{children}</div>
-          </main>
-        </div>
-      </div>
+        <main className="flex flex-col flex-1 overflow-x-hidden overflow-y-auto mx-6">
+          <header className="flex items-center sticky top-0 m-5">
+            <SidebarTrigger className="w-10 h-10" />
+            <Separator
+              orientation="vertical"
+              className="mr-3 ml-1 data-[orientation=vertical]:h-5"
+            />
+            <h1 className="text-xl font-semibold">Tire OCR Evaluation Tool</h1>
+          </header>
+
+          {children}
+        </main>
+      </SidebarProvider>
     </ThemeProvider>
   );
 };
