f3903c286030139a796d35d23b198113a4af1abf
Michal Musil <musilmichal16@gmail.com>
2026-04-23 16:57:17 +0200

    chore: remove obsolete prompts

--- src/AiPipeline.TireOcr.Ocr/AiPipeline.TireOcr.Ocr.Application/Repositories/IPromptRepository.cs
+++ src/AiPipeline.TireOcr.Ocr/AiPipeline.TireOcr.Ocr.Application/Repositories/IPromptRepository.cs
@@ -7,11 +7,6 @@ public interface IPromptRepository
     public Task<string> GetTaskPromptAsync();
     public Task<string> GetFewShotPromptAsync();
     public Task<string> GetCotPromptAsync();
-    public Task<string> GetBasePromptAsync();
-    public Task<string> GetBaseStrictPromptAsync();
-    public Task<string> GetPointsPromptAsync();
-    public Task<string> GetPointsStrictPromptAsync();
-    public Task<string> GetPointsStrictWithLisiContextPromptAsync();
     public Task<string> GetSpecializedDeepseekOcrPromptAsync();
     public Task<string> GetSpecializedHunyuanOcrPromptAsync();
     public Task<string> GetOcrEnginePromptAsync();
--- src/AiPipeline.TireOcr.Ocr/AiPipeline.TireOcr.Ocr.Infrastructure/Repositories/PromptRepositoryConfiguration.cs
+++ src/AiPipeline.TireOcr.Ocr/AiPipeline.TireOcr.Ocr.Infrastructure/Repositories/PromptRepositoryConfiguration.cs
@@ -14,7 +14,7 @@ public class PromptRepositoryConfiguration : IPromptRepository
 
     public async Task<string> GetMainPromptAsync(bool useRandomPrefix = false)
     {
-        var prompt = await GetOcrEngineWithManufacturerPromptAsync();
+        var prompt = await GetOcrEnginePromptAsync();
         if (useRandomPrefix)
             prompt = PrependRandomString(prompt);
         return prompt;
@@ -25,18 +25,6 @@ public class PromptRepositoryConfiguration : IPromptRepository
 
     public Task<string> GetFewShotPromptAsync() => Task.FromResult(GetPromptFromConfiguration("FewShot"));
     public Task<string> GetCotPromptAsync()=> Task.FromResult(GetPromptFromConfiguration("Cot"));
-
-    public Task<string> GetBasePromptAsync() => Task.FromResult(GetPromptFromConfiguration("Base"));
-
-    public Task<string> GetBaseStrictPromptAsync() => Task.FromResult(GetPromptFromConfiguration("BaseStrict"));
-
-    public Task<string> GetPointsPromptAsync() => Task.FromResult(GetPromptFromConfiguration("Points"));
-
-    public Task<string> GetPointsStrictPromptAsync() => Task.FromResult(GetPromptFromConfiguration("PointsStrict"));
-
-    public Task<string> GetPointsStrictWithLisiContextPromptAsync() =>
-        Task.FromResult(GetPromptFromConfiguration("PointsStrictLisiContext"));
-
     public Task<string> GetSpecializedDeepseekOcrPromptAsync() =>
         Task.FromResult(GetPromptFromConfiguration("DeepseekOcr"));
 
@@ -45,9 +33,6 @@ public class PromptRepositoryConfiguration : IPromptRepository
 
     public Task<string> GetOcrEnginePromptAsync() => Task.FromResult(GetPromptFromConfiguration("OcrEngine"));
     
-    private Task<string> GetOcrEngineWithManufacturerPromptAsync() => Task.FromResult(GetPromptFromConfiguration("OcrEngineWithManufacturer"));
-
-
     private string GetPromptFromConfiguration(string promptKey)
     {
         return _configuration.GetValue<string>($"Prompts:{promptKey}")!;
